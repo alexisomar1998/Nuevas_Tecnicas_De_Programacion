8b3aea8f6690ddffa22b1e1cfc15117ec427cc1c
Alexis Analuisa <alexis_analuisa1998@hotmail.com>
2021-06-24 12:01:35 -0500

    entrar

--- modulo_1/programming/01_Projects/index.js
+++ modulo_1/programming/01_Projects/index.js
@@ -1,12 +1,38 @@
-const express = require('express'); //requiero el paquete express
+const express = require("express"); //requiero el paquete express
 
-//genero una aplicacion express 
-const app = express()
+//genero una aplicacion express
+const app = express();
 //endpoint
-app.get('/', (req, res) => {
-    res.send("HOLA MUNDO, con ExpressJS")
-})
+app.get("/", (req, res) => {
+  res.send("HOLA MUNDO, con ExpressJs");
+});
+app.get("/saludo", (req, res) => {
+  //req.query
+  //console.log(rq.query);
+  //obtenemos la query del objeto request
+  const {
+    query: { nombre, apellido },
+  } = req;
+  /* segunda forma
+  const nombre = req.query.nombre;
+  const apellido = req.queery.apellido;
+  */
+
+  res.json({
+    Saludo: `Hola soy ${nombre} ${apellido}`,
+  });
+});
+
+app.get("/saludo/:nombre", (req, res) => {
+  const {
+    params: { nombre },
+  } = req ;
+  //segunda forma para params: const nombre = req.params.nombre
+  res.json({
+    nombre:nombre,
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Servidor escuchando en http://localhost:3000")
-})
+  console.log("Servidor escuchando en http://localhost:3000");
+});
---
+++ modulo_1/programming/01_Projects/routes/index.js
@@ -0,0 +1,11 @@
+//traemos el paquete
+const { Router } = require("express");
+
+//Inicializamos Router
+const router=Router()
+
+//creacion de los endpoint
+
+
+//Exportamos las ruta
+module.exports.RouterIndex = router 
