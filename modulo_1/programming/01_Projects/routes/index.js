fe400d416f19429c91f6577e9728e567063538d5
Alexis Analuisa <alexis_analuisa1998@hotmail.com>
2021-06-25 11:50:50 -0500

    entrar

--- modulo_1/programming/01_Projects/routes/index.js
+++ modulo_1/programming/01_Projects/routes/index.js
@@ -2,10 +2,9 @@
 const { Router } = require("express");
 
 //Inicializamos Router
-const router=Router()
-
+const router = Router()
 //creacion de los endpoint
 
 
 //Exportamos las ruta
-module.exports.RouterIndex = router 
+module.exports.RouterIndex = router
