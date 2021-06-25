9623decfb62950fd1fdda1df125e8b2c472a60be
Alexis Analuisa <alexis_analuisa1998@hotmail.com>
2021-06-25 13:15:49 -0500

    entrar

--- modulo_1/programming/01_Projects/index.js
+++ modulo_1/programming/01_Projects/index.js
@@ -1,38 +1,20 @@
 const express = require("express"); //requiero el paquete express
-
+const { Router } = require('express');
 //genero una aplicacion express
 const app = express();
-//endpoint
-app.get("/", (req, res) => {
-  res.send("HOLA MUNDO, con ExpressJs");
-});
-app.get("/saludo", (req, res) => {
-  //req.query
-  //console.log(rq.query);
-  //obtenemos la query del objeto request
-  const {
-    query: { nombre, apellido },
-  } = req;
-  /* segunda forma
-  const nombre = req.query.nombre;
-  const apellido = req.queery.apellido;
-  */
+//router
+const { RouterIndex } = require('./routes/index')
+//app.use("/", RouterIndex)
+// app.use("/user", RouterIndex)
+app.use("/user/data", RouterIndex)
 
-  res.json({
-    Saludo: `Hola soy ${nombre} ${apellido}`,
-  });
-});
-
-app.get("/saludo/:nombre", (req, res) => {
-  const {
-    params: { nombre },
-  } = req ;
-  //segunda forma para params: const nombre = req.params.nombre
-  res.json({
-    nombre:nombre,
-  });
-});
-
+/*
+        "/"+"/"="//" => "/"
+        "/"+"saludo" = "//Saludo" => "/saludo"
+        "/"+ "'/saludo/:nombre'" = //saludo/:nombre => "/saludo/:nombre"
+        "/user" + "/saludo" = "/user/saludo"
+        "user/data" + "/saludo" = "/user/data/saludo"
+*/
 app.listen(3000, () => {
   console.log("Servidor escuchando en http://localhost:3000");
 });
--- modulo_1/programming/01_Projects/routes/index.js
+++ modulo_1/programming/01_Projects/routes/index.js
@@ -4,7 +4,33 @@ const { Router } = require("express");
 //Inicializamos Router
 const router = Router()
 //creacion de los endpoint
-
+router
+    .get("/",(req, res) => {
+    res.send("Hola mundo con ExpressJS");
+    }).get("/saludo", (req, res) => {
+        //req.query
+        //console.log(rq.query);
+        //obtenemos la query del objeto request
+        const {
+          query: { nombre, apellido },
+        } = req;
+        /* segunda forma
+        const nombre = req.query.nombre;
+        const apellido = req.queery.apellido;
+        */
+      
+        res.json({
+          Saludo: `Hola soy ${nombre} ${apellido}`,
+        });
+      }).get("/saludo/:nombre", (req, res) => {
+        const {
+          params: { nombre },
+        } = req ;
+        //segunda forma para params: const nombre = req.params.nombre
+        res.json({
+          nombre:nombre,
+        });
+      })
 
 //Exportamos las ruta
 module.exports.RouterIndex = router
